5dad4137cd54cf4cf8e1fee45155556dd70c398a
Przemek <sienkowskip@gmail.com>
2015-05-13 17:38:08 +0200

    switched off sidekiq

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,6 @@ module Crm
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.active_job.queue_adapter = :sidekiq
+    #config.active_job.queue_adapter = :sidekiq
   end
 end
