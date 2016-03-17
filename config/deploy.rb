ad5dda3d46bf79852ffe2e67eb857e8552511aa4
Przemek Sienkowski <psienkowski@pgs-soft.com>
2016-03-17 19:08:46 +0100

    excluded bin directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :pty, true
 set :linked_files, %w{config/database.yml config/sidekiq.yml config/local_env.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
