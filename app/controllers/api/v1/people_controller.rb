043770445ac10ec2c3efee90dbf4e968d823ff1a
Przemek Sienkowski <psienkowski@pgs-soft.com>
2016-04-10 18:05:05 +0200

    Removed increasing response time

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -6,14 +6,6 @@ class Api::V1::PeopleController < ApplicationController
   expose(:person) { Person.find(params[:id]) }
 
   def index
-    rnd = (rand 1..23)
-    session[:stop] = 0
-    session[:stop] = 1 if (rnd ==20) && !(session[:stop] == 2)
-    if session[:stop] == 1
-      sleep((4*0.6).seconds)
-      session[:stop] == 2 
-    end
-
     if params[:search].eql?('search') && params[:person].present?
       respond_with Person.search(params[:person])
     else
