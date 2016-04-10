9b5f95abc36fb5230cd3fb5aa327f888da010e4f
Przemek Sienkowski <psienkowski@pgs-soft.com>
2016-04-10 15:13:38 +0200

    increased probability long response

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::PeopleController < ApplicationController
 
   def index
     rnd = (rand 1..23)
-    sleep((4*0.6).seconds) if rnd ==20
+    sleep((4*0.6).seconds) if rnd ==20 || rnd == 10 || rnd == 23
     if params[:search].eql?('search') && params[:person].present?
       respond_with Person.search(params[:person])
     else
